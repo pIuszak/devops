4566f7de4f65cb54b34e2f3f83ab7332e408ce93
pIuszak <luszukpawel@gmail.com>
2020-04-05 09:51:41 +0200

    [lab4] nwd

---
+++ nwd/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const redis = require("redis");
+const process = require("process");
+
+const app = express();
+
+const client = redis.createClient({
+    host: "redis-server",
+    port: 6379
+});
+
+function NWD(a, b) {
+    return b ? NWD(b, a % b) : a
+}
+
+client.set('nwd', 0);
+
+app.get('/', (req, res) => {
+    //res.send("XD");
+    client.get('nwd', (err, a_val, b_val) => {
+        resp.send('NWD: ' + a_val + " " + b_val + " = " + NWD(a_val, b_val));
+        client.set('counter', NWD(a_val, b_val));
+    });
+});
+
+app.listen(8080, () => {
+    console.log("Listening to port 8080")
+});
