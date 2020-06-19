786bb24827a8d9acbb5f97958458bbb5e8af760c
pluszak <luszukpawel@gmail.com>
2020-06-19 23:56:04 +0200

    [fix] folder hierarchy

--- project-breathalyzer/clear.sh
+++
@@ -1,4 +0,0 @@
-kubectl delete svc --all
-kubectl delete deploy --all
-kubectl delete pods --all
-kubectl get all
--- project-breathalyzer/res.sh
+++
@@ -1,5 +0,0 @@
-kubectl delete svc --all
-kubectl delete deploy --all
-kubectl delete pods --all
-kubectl apply -f .
-kubectl get all
